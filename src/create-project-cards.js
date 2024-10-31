e65f9c36915d940ac5d561d50461a73566e9b010
robert thornton <robert.thornton92@gmail.com>
2024-10-31 21:00:09 +0200

    Added a lorem ipsum function that adds lorem as a placeholder for project info. This helps create a tangible layout.

--- src/create-project-cards.js
+++ src/create-project-cards.js
@@ -1,11 +1,11 @@
 import { createCard } from './create-card';
 
-export default function createProjectCards(projects) {
-    const project_cnt = document.createElement('div');
+export default function createProjectCards(projects, lorem) {
+    const project_cnt = document.createElement('div');  
     project_cnt.id = 'project-container';
+    
     Array.from(projects.projects).forEach(_project => {
-        console.log(_project.name)
-        const project = createCard(_project.name, _project.description);
+        const project = createCard(_project.name, lorem ? lorem.generateSentences(1) : _project.description);
         project_cnt.appendChild(project);
     });
     return project_cnt;
