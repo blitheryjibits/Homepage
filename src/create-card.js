6e5f24394843cb7e2935cc692821f1200153d36a
robert thornton <robert.thornton92@gmail.com>
2024-10-31 20:58:10 +0200

    Added the main layout style for the project cards with placeholder for images, title, and project info.

--- src/create-card.js
+++ src/create-card.js
@@ -1,7 +1,25 @@
 
 export function createCard(title, content) {
     const card = document.createElement('div');
-    card.className = 'card';
-    card.innerHTML = `<h3>${title}</h3><p>${content}</p>`;
+    const project_img_cnt = document.createElement('div');
+    const _title = document.createElement('h3');
+    const desc = document.createElement('p');
+    const desc_cnt = document.createElement('div');
+    
+    project_img_cnt.classList.add('project-img-cnt');
+    card.classList.add('card');
+    _title.classList.add('title');
+    desc.classList.add('project-desription');
+
+    _title.textContent = title;
+    desc.textContent = content;
+    
+    // temporary filler text for image
+    project_img_cnt.innerHTML = "<h3>Placeholder</h3>"
+
+    desc_cnt.append(_title, desc);
+    card.append(project_img_cnt, desc_cnt);
+
+
     return card;
 }
