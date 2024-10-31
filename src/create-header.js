d07a11b9ef5798da01955311456b8fe1db87f1d8
robert thornton <robert.thornton92@gmail.com>
2024-10-31 20:51:46 +0200

    Created a new module with a default export function that creates the main header for the page.

---
+++ src/create-header.js
@@ -0,0 +1,22 @@
+
+export default function createHeader(lorem) {
+    const element = document.createElement('div');
+    const imgCnt = document.createElement('div');
+    const name = document.createElement('p');
+    const abtme = document.createElement('div');
+    const infoCnt = document.createElement('div');
+
+    name.innerHTML = "Robert<br>&nbsp&nbspThornton"; // Note to sanitize using DOMPurify if this changes to user input
+    abtme.innerText = lorem.generateParagraphs(1);
+
+    imgCnt.classList.add('img-cnt');
+    abtme.classList.add('about-me');
+    name.classList.add('name');
+    infoCnt.classList.add('info-cnt');
+
+    imgCnt.appendChild(name);
+    element.append(imgCnt, abtme);
+    infoCnt.append(element)
+
+    return infoCnt;
+}
