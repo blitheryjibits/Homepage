1211ba35175bfca01a365d996e5207e30cbda61d
robert thornton <robert.thornton92@gmail.com>
2024-10-31 20:47:36 +0200

    Added module rule for image handling.

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   	entry: './src/index.js',
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
    	 	clean: true,
   	},
   	module: {
     		rules: [
-      		{
+      	{
         	test: /\.css$/i,
         	use: ['style-loader', 'css-loader'],
-      },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
     ],
   },
   plugins: [
