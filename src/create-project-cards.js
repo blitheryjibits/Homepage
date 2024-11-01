727634ff0bea691cb251b0c53514d6f385533beb
robert thornton <robert.thornton92@gmail.com>
2024-11-01 14:14:34 +0200

    Refactor. Used prettier to enhance readability.

--- src/create-project-cards.js
+++ src/create-project-cards.js
@@ -1,12 +1,15 @@
-import { createCard } from './create-card';
+import { createCard } from "./create-card";
 
 export default function createProjectCards(projects, lorem) {
-    const project_cnt = document.createElement('div');  
-    project_cnt.id = 'project-container';
-    
-    Array.from(projects.projects).forEach(_project => {
-        const project = createCard(_project.name, lorem ? lorem.generateSentences(1) : _project.description);
-        project_cnt.appendChild(project);
-    });
-    return project_cnt;
+  const project_cnt = document.createElement("div");
+  project_cnt.id = "project-container";
+
+  Array.from(projects.projects).forEach((_project) => {
+    const project = createCard(
+      _project.name,
+      lorem ? lorem.generateSentences(1) : _project.description,
+    );
+    project_cnt.appendChild(project);
+  });
+  return project_cnt;
 }
